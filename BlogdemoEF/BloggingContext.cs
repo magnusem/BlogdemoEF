21beab3bbaad0a7ac3bdf5e3576af005841472db
Magnus Emil Mortensen <magn1596@edu.easj.dk>
2017-03-07 13:17:55 +0100

    oprettet database og indsat værdier

---
+++ BlogdemoEF/BloggingContext.cs
@@ -0,0 +1,22 @@
+namespace BlogdemoEF
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class BloggingContext : DbContext
+    {
+        public BloggingContext()
+            : base("name=BloggingContext")
+        {
+        }
+
+        public virtual DbSet<Blogs> Blogs { get; set; }
+        public virtual DbSet<Posts> Posts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
